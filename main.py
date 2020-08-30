22ad1266b3a1d85832daeede0a14584f942c6a42
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-30 08:31:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# my_lst = [6, 8, 9, 1, 2, 5]
+
+# for idx, num in enumerate(my_lst):
+#     print(idx, num)
+
+
+# my_lst = [6, 8, 9, 1, 2, 5]
+
+# for idx, num in enumerate(my_lst, -62):
+#     print(idx, num)
+
+
+my_list = list(range(1, 101))
+for idx, num in enumerate(my_list):
+    print(idx / num)
